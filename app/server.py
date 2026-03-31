c3f779da569c83e1e5b56f71974116bf50b7470f
hoopoe404 <mdhabibhossainnoor@gmail.com>
2026-04-01 02:23:08 +0600

    Changed the file main.py to server.py

--- app/server.py
+++ app/server.py
@@ -86,4 +86,4 @@ async def public_settings():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=settings.port, reload=True)
+    uvicorn.run("app.server:app", host="0.0.0.0", port=settings.port, reload=True)
